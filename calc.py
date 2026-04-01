20fa726f148fe38061dc7247c583b2b69294eb43
Fares Elkordy <felk0002@student.monash.edu>
2026-04-01 18:35:05 +1100

    Return None, not 0, on invalid input

--- calc.py
+++ calc.py
@@ -5,8 +5,7 @@ def compute(expression):
     num0, num1 = int(num0), int(num1)
     if operator == '+':
         return num0 + num1
-    else:
-        print('unknown operator!')
-        return None
+    print('unknown operator!')
+    return None
 
 print(compute('5 + 8'))
